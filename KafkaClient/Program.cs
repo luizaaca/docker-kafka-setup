e3797e576ff925ff158c52e0f438a4af8f852d2d
Luiz Assis <luizaaca@gmail.com>
2020-03-28 18:50:34 -0300

    testing 2 topics

--- KafkaClient/Program.cs
+++ KafkaClient/Program.cs
@@ -2,6 +2,7 @@
 using KafkaNet.Model;
 using System;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace KafkaClient
 {
@@ -9,13 +10,29 @@ namespace KafkaClient
     {
         static void Main(string[] args)
         {
-            string topic = "TestTopic";
+            string topic = "TestTopic-1";
             Uri uri = new Uri("http://192.168.99.100:9092");
             var options = new KafkaOptions(uri);
             var router = new BrokerRouter(options);
-            var consumer = new Consumer(new ConsumerOptions(topic, router));
-            foreach (var message in consumer.Consume())
-                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+            var consumer1 = new Consumer(new ConsumerOptions(topic, router));
+            Task.Run(() =>
+            {
+                foreach (var message in consumer1.Consume())
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+                    //Console.WriteLine("Response: P{0},O{1} : {2}",
+                    //    message.Meta.PartitionId,
+                    //    message.Meta.Offset,
+                    //    message.Value);
+                }
+            });
+            topic = "TestTopic-2";
+            var consumer2 = new Consumer(new ConsumerOptions(topic, router));
+            Task.Run(() =>
+            {
+                foreach (var message in consumer2.Consume())
+                    Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+            });
             Console.ReadLine();
         }
     }
--- KafkaPublisher/Program.cs
+++ KafkaPublisher/Program.cs
@@ -12,13 +12,24 @@ namespace KafkaPublisher
         {
             Uri uri = new Uri("http://192.168.99.100:9092");
             var options = new KafkaOptions(uri);
-            string topic = "TestTopic";
+            string topic = "TestTopic-1";
             var router = new BrokerRouter(options);
             var client = new Producer(router);
 
             for (int i = 0; i < 10; i++)
             {
-                string payload = $"Welcome to Kafka {i+1}!";
+                string payload = $"Welcome to Kafka {i+1}! - {topic}";
+                Message msg = new Message(payload);
+                client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            }
+
+            topic = "TestTopic-2";
+            router = new BrokerRouter(options);
+            client = new Producer(router);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string payload = $"Welcome to Kafka {i + 1} - {topic}!";
                 Message msg = new Message(payload);
                 client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
             }

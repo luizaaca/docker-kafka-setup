ccc3ac69cafa89eb64f6e8fee2eec129e4e6d96b
Luiz Assis <luizaaca@gmail.com>
2020-03-28 17:39:56 -0300

    init

---
+++ KafkaClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using System;
+using System.Text;
+
+namespace KafkaClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topic = "TestTopic";
+            Uri uri = new Uri("http://192.168.99.100:9092");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var consumer = new Consumer(new ConsumerOptions(topic, router));
+            foreach (var message in consumer.Consume())
+                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KafkaPublisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using System;
+using System.Collections.Generic;
+
+namespace KafkaPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://192.168.99.100:9092");
+            var options = new KafkaOptions(uri);
+            string topic = "TestTopic";
+            var router = new BrokerRouter(options);
+            var client = new Producer(router);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string payload = $"Welcome to Kafka {i+1}!";
+                Message msg = new Message(payload);
+                client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

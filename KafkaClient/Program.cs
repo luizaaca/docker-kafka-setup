24d171a4d1508db442303daaf7e32bf338f3998e
Luiz Assis <luizaaca@gmail.com>
2020-03-31 14:36:10 -0300

    adding debezium connector and command config

--- KafkaClient/Program.cs
+++ KafkaClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Confluent.Kafka;
+using Newtonsoft.Json;
 using System;
 
 namespace KafkaClient
@@ -7,12 +8,13 @@ namespace KafkaClient
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Consumer {args[0]}.{args[1]} iniciado.");
             var config = new ConsumerConfig
             {
                 BootstrapServers = "192.168.99.100:9092",
-                ClientId = "Consumer",
+                ClientId = args[0],
                 AutoOffsetReset = AutoOffsetReset.Latest,
-                GroupId = "TestConfluent",
+                GroupId = args[1],
                 //AutoCommitIntervalMs = 5000
                 //EnableAutoOffsetStore = false,
                 //EnableAutoCommit = true,
@@ -21,26 +23,29 @@ namespace KafkaClient
 
             using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
             {
-                consumer.Subscribe("Topic");
+                consumer.Subscribe(args[2]);
                 //count to check if consumer restarts from last read message in the broker
                 //run again this consumer after 50 messages
-                var count = 0;
-                while (count < 50)
+                var count = args[3] != null ? int.Parse(args[3]) : 10;
+
+                while (count > 0)
                 {
                     var consumeResult = consumer.Consume();
 
-                    if (consumeResult.Message == null) break;
+                    dynamic result = JsonConvert.DeserializeObject(consumeResult.Value);
 
-                    Console.WriteLine(consumeResult.Value);
+                    Console.WriteLine($"Consumer {args[0]}.{args[1]} recebeu msg.");
+                    Console.WriteLine($"Id = {result.payload.after.id} Prop = {result.payload.after.prop} em {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}");
 
                     //consumer.StoreOffset(consumeResult);
                     consumer.Commit(consumeResult);
-                    count++;
+
+                    count--;
                 }
 
                 consumer.Close();
             }
-
+            Console.WriteLine($"Consumer {args[0]}.{args[1]} encerrado.");
             Console.ReadLine();
         }
     }
